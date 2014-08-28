8b19796c7b4982fc90ffa2f9f1863491fd4fa325
Владимир Иванов <zlumyo@gmal.com>
2014-08-28 17:21:53 +0400

    Переделана реализация API объясняющего графа таким образом для генерации id элементов в более ООП стиле. Реализован под это дело ассерт \b.

--- egraph/egraph.py
+++ egraph/egraph.py
@@ -8,8 +8,8 @@ class Part(metaclass=abc.ABCMeta):
     Абстрактная часть регулярного выражения.
     """
 
-    def __init__(self):
-        self._id = None
+    def __init__(self, id=None):
+        self._id = id
 
     @property
     def id(self):
@@ -30,20 +30,15 @@ class PartContainer(Part, metaclass=abc.ABCMeta):
     (по сути конкатенация)
     """
 
-    def __init__(self):
+    def __init__(self, id=None):
+        Part.__init__(self, id)
         self._parts = []
-        self._gmain = None
 
     def add_part(self, part):
-        if part.id is None:
-            part.id = self._gmain._idcounter
-        self._gmain._idcounter += 1
-        if isinstance(part, PartContainer):
-            part._gmain = self._gmain
         self._parts.append(part)
 
     @abc.abstractmethod
-    def to_graph(self):
+    def to_graph(self, current=None, id_counter=1, label=''):
         pass
 
 
@@ -53,29 +48,29 @@ class ExplainingGraph(PartContainer):
     """
 
     def __init__(self):
-        PartContainer.__init__(self)
-        self._id = "explaining_graph"
-        self._gmain = self
-        self._idcounter = 1
+        PartContainer.__init__(self, "explaining_graph")
+        self._id_counter = 1
 
     def to_graph(self):
-        graph = DotDigraph()
+        graph = DotDigraph(self._id)
 
-        begin = DotNode(self._idcounter, "begin", 'filled', "purple", "begin", "rect", "purple")
-        self._idcounter += 1
+        begin = DotNode(self._id_counter, "begin", 'filled', "purple", "begin", "rect", "purple")
+        self._id_counter += 1
         graph.items.append(begin)
-        end = DotNode(self._idcounter, "end", 'filled', "purple", "end", "rect", "purple")
-        self._idcounter += 1
+        end = DotNode(self._id_counter, "end", 'filled', "purple", "end", "rect", "purple")
+        self._id_counter += 1
         graph.items.append(end)
 
         current = begin
+        label = ''
         for item in self._parts:
-            parts, new = item.to_graph(current)
-            graph.items += parts + [DotLink(current, new, self._idcounter)]
-            self._idcounter += 1
-            current = new
+            parts, new_current, new_id, label = item.to_graph(current=current, label=label, id_counter=self._id_counter)
+            graph.items += parts
+            self._id_counter = new_id
+            current = new_current
 
-        graph.items += [DotLink(current, end, self._idcounter)]
+        graph.items.append(DotLink(current, end, self._id_counter))
+        self._id_counter += 1
 
         return graph
 
@@ -91,20 +86,29 @@ class Alternative(Part):
 
     def to_graph(self):
         pass
-        
+
 
 class Text(Part):
     """
     Простой текст в регулярном выражении.
     """
 
-    def __init__(self, txt=""):
+    def __init__(self, txt="", id=None):
         Part.__init__(self)
         self.text = txt
 
-    def to_graph(self, current=None):
+    def to_graph(self, current=None, id_counter=1, label=''):
+        if self._id is None:
+            self._id = id_counter
+            id_counter += 1
         node = DotNode(self._id, self.text)
-        return [node], node
+        result = [node]
+        if current is not None:
+            # noinspection PyTypeChecker
+            result.append(DotLink(current, node, id_counter, label))
+            id_counter += 1
+        current = node
+        return result, current, id_counter, ""
 
 
 class BorderAssert(Part):
@@ -116,8 +120,10 @@ class BorderAssert(Part):
         Part.__init__(self)
         self.inverse = inverse
 
-    def to_graph(self):
-        pass
+    def to_graph(self, current=None, id_counter=1, label=''):
+        text = "a word boundary" if not self.inverse else "not a word boundary"
+        new_label = text if label == '' else label + '\\n' + text
+        return [], current, id_counter, new_label
 
 
 class Subexpression(PartContainer):
